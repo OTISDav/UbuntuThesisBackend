42055c794e9610c27b7a758a0fe7a6ef4b423fbf
BOTCHOLI E. DAVID <159715854+OTISDav@users.noreply.github.com>
2025-02-05 12:56:34 +0000

    Update urls.py

--- theses/urls.py
+++ theses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ThesisViewSet, FavoriteViewSet, AnnotationViewSet, SuggestionsView
+from .views import ThesisViewSet, FavoriteViewSet, AnnotationViewSet, SuggestionsView, download_file
 
 router = DefaultRouter()
 router.register(r'theses', ThesisViewSet, basename='thesis')
@@ -10,4 +10,5 @@ router.register(r'annotations', AnnotationViewSet, basename='annotation')
 urlpatterns = [
     path('', include(router.urls)),
     path('suggestions/', SuggestionsView.as_view({'get': 'list'}), name='suggestions'),
+    path('download/<str:file_name>/', views.download_file, name='download_file'),
 ]
