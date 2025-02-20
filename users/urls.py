89a78345a04811785ca73a229dcd9eb9ca0fec7e
BOTCHOLI E. DAVID <159715854+OTISDav@users.noreply.github.com>
2025-02-20 07:44:06 +0000

    Update urls.py
    profile

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, NotificationPreferenceView
+from .views import RegisterView, LoginView, LogoutView, NotificationPreferenceView, ProfileDetailView
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('auth/profile/', ProfileDetailView.as_view(), name='profile'),
     path('notifications/preferences/', NotificationPreferenceView.as_view(), name='notification-preferences'),
 ]
