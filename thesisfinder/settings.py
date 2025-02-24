fa1b20b10bddee4ab4e9f2d14caa1e10046d97dd
OTISDav <159715854+OTISDav@users.noreply.github.com>
2025-02-24 23:55:35 +0000

    profile

--- thesisfinder/settings.py
+++ thesisfinder/settings.py
@@ -133,19 +133,16 @@ import dj_database_url
 #}
 
 # Add these at the top of your settings.py
-import os
-from dotenv import load_dotenv
+
+
 from urllib.parse import urlparse
 
-load_dotenv()
-
-# Replace the DATABASES section of your settings.py with this
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
+        'NAME': tmpPostgres.path.decode().replace('/', ''),
         'USER': tmpPostgres.username,
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
