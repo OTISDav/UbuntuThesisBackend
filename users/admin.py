d353e9282e9bf74f12c1c7de58085780893bd6c6
OTISDav <159715854+OTISDav@users.noreply.github.com>
2025-08-02 18:19:41 +0000

    admin.py de users

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import CustomUser
+from django.contrib.auth import get_user_model
+from .models import Profile, NotificationPreference
 
-class CustomUserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email', 'password')
-    search_fields = ('username', 'email', 'password')
-    list_filter = ('is_staff', 'is_active')
+User = get_user_model()
 
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_verified', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('is_verified', 'is_staff', 'is_active')
+    ordering = ('id',)
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_picture_display')
+    search_fields = ('user__username', 'user__email')
 
-admin.site.register(CustomUser, CustomUserAdmin)
-# Register your models here.
+    def profile_picture_display(self, obj):
+        if obj.profile_picture:
+            return obj.profile_picture.url
+        return "-"
+    profile_picture_display.short_description = 'Photo de profil'
+
+@admin.register(NotificationPreference)
+class NotificationPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email_notifications', 'push_notifications', 'sms_notifications')
+    search_fields = ('user__username', 'user__email')
+
